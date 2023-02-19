f598a56e01cf6cab30c33a10f075061dcd2ce558
VChelondaev <104819871+VChelondaev@users.noreply.github.com>
2023-02-20 00:39:57 +0800

    Add files via upload

--- Array.c
+++ Array.c
@@ -3,34 +3,49 @@
 #include <math.h>
 #include <stdio.h>
 #include <malloc.h>
-
-
+#include <omp.h>
 
 #define TYPE double
+
+#ifdef FLOAT
+#define TYPE float
+#endif // FLOAT
+
 #define BASE 10000000
 
 //TYPE array[BASE] = { 0 };
 
 int main() {
 
-
-		TYPE sum = 0;
-		TYPE* array = (TYPE*)malloc(BASE * sizeof(TYPE));
-#pragma acc kernels
-	{
-		for (int i = 0; i < BASE; i++) {
-			array[i] = sin(2 * M_PI * i / BASE);
-		}
-	}
-#pragma acc kernels
-	{
-		for (int i = 0; i < BASE; i++) {
-			sum += array[i];
-		}
-		printf("%lf", sum);
-	}
-
 	
+	TYPE sum = 0;
+	TYPE* arr = (TYPE*)malloc(BASE * sizeof(TYPE));
+	double wtime;
+	#pragma acc data create(arr[:N])
+	#pragma acc kernels
+	{		
+		double wtime_start = omp_get_wtime();
+		for (int i = 0; i < BASE; i++) {
+			arr[i] = sin(2 * M_PI * i / BASE);
+		}
+		double wtime_end = omp_get_wtime();
+		wtime = wtime_end - wtime_start;
+		
+	}
+	printf("to fill: %lf\n", wtime);
+	#pragma acc kernels
+	{
+		double wtime_start = omp_get_wtime();
+		for (int i = 0; i < BASE; i++) {
+			sum += arr[i];
+		}
+		double wtime_end = omp_get_wtime();
+		wtime = wtime_end - wtime_start;
+		
+	}
+	printf("to sum: %lf\n", wtime);
+	
+	printf("%lf\n", sum);
 
 	return 0;
 }
