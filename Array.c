76dfc19ac619484457c629a9f2debea4c0f0291d
VChelondaev <104819871+VChelondaev@users.noreply.github.com>
2023-02-20 02:49:03 +0800

    Add files via upload

--- Array.c
+++ Array.c
@@ -13,38 +13,35 @@
 
 #define BASE 10000000
 
-//TYPE array[BASE] = { 0 };
-
 int main() {
 
-	
 	TYPE sum = 0;
 	TYPE* arr = (TYPE*)malloc(BASE * sizeof(TYPE));
-	double wtime;
-	#pragma acc data create(arr[:N])
-	#pragma acc kernels
-	{		
-		double wtime_start = omp_get_wtime();
+
+	double wtime, wtime_start, wtime_end;
+
+	#pragma acc data create(arr[:BASE]) copy(sum) copyin(BASE)
+	{
+		wtime_start = omp_get_wtime();
+		#pragma acc parallel loop vector vector_length(160) gang
 		for (int i = 0; i < BASE; i++) {
 			arr[i] = sin(2 * M_PI * i / BASE);
 		}
-		double wtime_end = omp_get_wtime();
+		wtime_end = omp_get_wtime();
 		wtime = wtime_end - wtime_start;
-		
-	}
-	printf("to fill: %lf\n", wtime);
-	#pragma acc kernels
-	{
-		double wtime_start = omp_get_wtime();
+
+		printf("to fill: %lf\n", wtime);
+
+		wtime_start = omp_get_wtime();
+		#pragma acc parallel loop reduction(+:sum) vector vector_length(160) gang
 		for (int i = 0; i < BASE; i++) {
 			sum += arr[i];
 		}
-		double wtime_end = omp_get_wtime();
+		wtime_end = omp_get_wtime();
 		wtime = wtime_end - wtime_start;
-		
 	}
+
 	printf("to sum: %lf\n", wtime);
-	
 	printf("%lf\n", sum);
 
 	return 0;
